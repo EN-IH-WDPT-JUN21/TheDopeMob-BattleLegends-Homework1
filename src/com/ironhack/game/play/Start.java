40778b8f79a497a83164cdbe762ab60c8c85cbb5
Jegor <jegor.chomczuk@gmail.com>
2021-07-06 12:00:53 +0200

    New method playAgain() in Start.java and its  entry point in Main.java

--- src/com/ironhack/game/play/Start.java
+++ src/com/ironhack/game/play/Start.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -24,6 +25,40 @@ public abstract class Start {
             }
         }
         System.out.println("GET READY FOR BATTLE!");
+    }
+
+    // Play again option
+    public static void playAgain() throws FileNotFoundException {
+        System.out.println("Play again?");
+        System.out.println("┎---------------------------------------------┒");
+        System.out.println("│[1] Yes                                      │");
+        System.out.println("│[2] No                                       │");
+        System.out.println("┖---------------------------------------------┚");
+        Scanner scanner = new Scanner(System.in);
+
+        int playAgain;
+        do {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Please enter [1] or [2].\n");
+                scanner.next();
+            }
+            playAgain = scanner.nextInt();
+
+            if (playAgain < 1 || playAgain > 2)
+                System.out.println("Please enter [1] or [2].\n");
+
+        } while (playAgain < 1 || playAgain > 2);
+
+        switch (playAgain) {
+            case 1:
+                Menu.menuControl(1);
+                break;
+            case 2:
+                System.out.println("Thank you for playing.\nNext legendary battles are waiting for you!");
+                break;
+            default:
+                System.out.println("\nThings went south. Try again.");
+        }
     }
 
 }
